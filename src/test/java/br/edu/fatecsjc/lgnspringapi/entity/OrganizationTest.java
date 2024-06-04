58cd5db59aeda8f76b8100ab35cf96457af8d484
Daniloel <zoltancao@gmail.com>
2024-06-04 00:27:07 -0300

    Criando classe de teste de Organization

---
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/entity/OrganizationTest.java
@@ -0,0 +1,47 @@
+package br.edu.fatecsjc.lgnspringapi.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OrganizationTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getInstitutionName() {
+    }
+
+    @Test
+    void getHeadquartersCountry() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setAddress() {
+    }
+
+    @Test
+    void setInstitutionName() {
+    }
+
+    @Test
+    void setHeadquartersCountry() {
+    }
+}

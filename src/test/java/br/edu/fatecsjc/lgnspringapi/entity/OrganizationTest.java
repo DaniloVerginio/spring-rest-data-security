05e8a26ac2fe9b74a0fe34049ad4028659eb6ceb
Daniloel <zoltancao@gmail.com>
2024-06-12 00:04:33 -0300

    Atualizada a Classe OrganatonTest

--- src/test/java/br/edu/fatecsjc/lgnspringapi/entity/OrganizationTest.java
+++ src/test/java/br/edu/fatecsjc/lgnspringapi/entity/OrganizationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class OrganizationTest {
+ class OrganizationTest {
 
     private Organization organization;
 
